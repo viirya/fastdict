40044d040786fbcaf1314fc422c788aa14c4d12c
viirya <viirya@gmail.com>
2013-12-07 22:40:45 +0800

    add more usage examples.

--- fastdict/fastdict.py
+++ fastdict/fastdict.py
@@ -11,5 +11,20 @@ print f_dict.get(123)[0]
 print f_dict.get(123)[0].first
 print f_dict.get(123)[0].second
  
+f_dict.append(123, 678, 'vec1')
+print f_dict.size()
 
+print f_dict.get(123)
+print f_dict.get(123)[0]
+print f_dict.get(123)[0].first
+print f_dict.get(123)[0].second
+print f_dict.get(123)[1]
+print f_dict.get(123)[1].first
+print f_dict.get(123)[1].second
 
+for ele in f_dict.get(123):
+    print ele
+    print ele.first
+    print ele.second
+
+ 
