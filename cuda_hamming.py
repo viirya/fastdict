c915702f6d91bd8ab2fa06834fae5531b4ef8d7b
viirya <viirya@gmail.com>
2014-01-04 16:16:39 +0800

    create branch for cuda implementation that compresses returned distances in small footprint.

--- cuda_hamming.py
+++ cuda_hamming.py
@@ -8,7 +8,7 @@ from pycuda.compiler import SourceModule
 
 class CudaHamming(object):
 
-    def __init__(self,  block = (64, 1, 1), grid = (100, 1)):
+    def __init__(self,  block = (500, 1, 1), grid = (2, 1)):
 
         vector_len = 100000
 
@@ -16,50 +16,94 @@ class CudaHamming(object):
 typedef unsigned int uint8_t;
 typedef unsigned long int uint32_t;
 typedef unsigned long long int uint64_t;
-__global__ void compressed_hamming_dist(uint64_t* query, uint64_t** bit_counts, uint64_t* max_length, uint64_t* distances)
+__global__ void compressed_hamming_dist(uint64_t* query, uint64_t** bit_counts, uint64_t* max_length, uint64_t* distances, uint64_t* base)
 {
-    const uint64_t i = gridDim.x * blockDim.x * blockIdx.y + blockIdx.x * blockDim.x + threadIdx.x;
-    
-    const uint64_t binary_code_id = i / (uint64_t)64;
+  const uint64_t i = gridDim.x * blockDim.x * blockIdx.y + blockIdx.x * blockDim.x + threadIdx.x;
 
-    __shared__ float share_distances[64];
-    
-    if (binary_code_id < max_length[0]) {
+  // how many binary codes we uncompress in a cuda thread
+  int batch_size = 100;
 
-        uint8_t column_index = i % 64;
+  uint64_t binary_codes[100] = {};
+  const uint64_t i_for_batch = i * batch_size;
+
+  if (i_for_batch < max_length[0]) {
+
+    for (uint32_t simple_index = 0; (simple_index < batch_size) && (i_for_batch + simple_index < max_length[0]); simple_index++) {
+        binary_codes[simple_index] = 0;
+    }
+
+    uint64_t binary_code = 0x00;
+    
+    for (uint64_t column_index = 0; column_index < 64; column_index++) {
+
         uint64_t count_for_bits = 0;
         uint64_t bit_count_index = 0;
         uint8_t bit_type = 0x00;
-        
-        uint64_t query_bit = query[0] & ((uint64_t)1 << column_index);
-        
+
+        // the index for currently uncompressing binary code
+        uint64_t current_binary_index = 0;
+
         while (count_for_bits <= max_length[0]) {
             count_for_bits += bit_counts[column_index][bit_count_index++];
-        
-            if (count_for_bits > binary_code_id) {
-                if (bit_type == 1) {
-                    if (query_bit == 0)
-                        share_distances[column_index]++;
-                } else {
-                    if (query_bit > 0)
-                        share_distances[column_index]++;
-                }
+
+            if (count_for_bits > max_length[0])
                 break;
+
+            while ((count_for_bits > (i_for_batch + current_binary_index)) && (current_binary_index < batch_size) && (i_for_batch + current_binary_index < max_length[0])) {
+                if (bit_type == 1) {
+                    binary_codes[current_binary_index] = binary_codes[current_binary_index] | ((uint64_t)1 << column_index);
+                }
+                // move to next binary code
+                current_binary_index++;
             } 
             bit_type = bit_type ^ 0x01;
         } 
 
-        __syncthreads();
+    } 
 
-        for (int sum_index = 64 / 2; sum_index != 0; sum_index /= 2)
-            if (column_index < sum_index)
-                share_distances[column_index] += share_distances[column_index + sum_index];
-            __syncthreads();
-        if (column_index == 0)
-            distances[binary_code_id] = share_distances[0];
+    uint64_t mask = 0x0000000000000000;
+    for (int mask_count = 0; mask_count < base[0]; mask_count++)
+        mask = (mask << 1) | 0x01;
+
+    for (int binary_code_index = 0; (binary_code_index < batch_size) && (i_for_batch + binary_code_index < max_length[0]); binary_code_index++) {
+
+        if (binary_codes[binary_code_index] > 0x00 || 1) {
+
+            uint64_t xor_r = query[0] ^ binary_codes[binary_code_index];
+            
+            const uint64_t m1  = 0x5555555555555555; 
+            const uint64_t m2  = 0x3333333333333333; 
+            const uint64_t m4  = 0x0f0f0f0f0f0f0f0f; 
+            const uint64_t m8  = 0x00ff00ff00ff00ff; 
+            const uint64_t m16 = 0x0000ffff0000ffff; 
+            const uint64_t m32 = 0x00000000ffffffff; 
+            const uint64_t hff = 0xffffffffffffffff; 
+            const uint64_t h01 = 0x0101010101010101; 
+            
+            xor_r -= (xor_r >> 1) & m1;    
+            xor_r = (xor_r & m2) + ((xor_r >> 2) & m2); 
+            xor_r = (xor_r + (xor_r >> 4)) & m4;        
+
+            uint32_t store_base = (i_for_batch + binary_code_index) / base[0];
+            uint32_t offset = (base[0] - 1) - (i_for_batch + binary_code_index) % base[0];           
+            uint64_t distance = (xor_r * h01) >> 56;
+
+            //if (distance < mask)
+            //    distance = distance & mask;
+
+            if (distance > mask)
+                distance = mask;
  
+            distance = distance << (offset * base[0]);
+            distances[i_for_batch + binary_code_index] = distances[i_for_batch + binary_code_index] | distance;
 
+            //distances[i_for_batch + binary_code_index] = (xor_r * h01) >> 56;
+            //distances[i_for_batch + binary_code_index] = tmp_binary_codes[binary_code_index];
+            //distances[i_for_batch + binary_code_index] = binary_codes[binary_code_index];
+            //distances[i_for_batch + binary_code_index] = i_for_batch + binary_code_index + 1;
+        }
     }
+  }
 }
         """)
         
@@ -112,7 +156,7 @@ __global__ void hamming_dist(uint64_t *a, uint64_t *b, uint64_t *length)
     def cuda_hamming_dist_in_compressed_domain(self, vec_a, compressed_columns, image_ids):
 
         binary_code_length = len(image_ids)
-
+        binary_code_length = 100
         self.benchmark_begin('preparing')
 
         addresses = [] 
@@ -131,26 +175,29 @@ __global__ void hamming_dist(uint64_t *a, uint64_t *b, uint64_t *length)
 
         drv.memcpy_htod(arrays_gpu, np_addresses)
 
-        distances = numpy.zeros(binary_code_length).astype(numpy.uint64)
+        distance_compress_base = 4
 
-        #binary_code_length = 10000
+        distances = numpy.zeros(binary_code_length).astype(numpy.uint64)
  
         length = numpy.array([binary_code_length]).astype(numpy.uint64)
  
+        base = numpy.array([distance_compress_base]).astype(numpy.uint64)
+ 
         print "total: " + str(binary_code_length) + " compressed binary codes." 
+        print "compressed to " + str(binary_code_length / (64 / distance_compress_base)) + " distances."
 
         self.benchmark_end('preparing')
         self.benchmark_begin('cudaing')
         self.compressed_hamming_dist(
-                drv.In(vec_a), arrays_gpu, drv.In(length), drv.InOut(distances),
+                drv.In(vec_a), arrays_gpu, drv.In(length), drv.Out(distances), drv.In(base),
                 block = self.block, grid = self.grid)
         self.benchmark_end('cudaing')
 
         print distances
         count = 0
-        for dis in distances:
-            print "count: " + str(count) + " " + str(dis) + " image: " + str(image_ids[count])
-            count += 1
+        #for dis in distances:
+        #    print "count: " + str(count) + " " + str(dis)
+        #    count += 1
         print distances.shape
 
     def multi_iteration(self, vec_a, vec_b):
