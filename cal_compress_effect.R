03a1859e2604c89610f92d4cb1a36429109c1d64
Liang-Chi Hsieh <viirya@gmail.com>
2014-03-08 14:15:55 +0800

    add parameter for setting sampled dimensions.

--- cal_compress_effect.R
+++ cal_compress_effect.R
@@ -1,27 +1,32 @@
 
 arg <- commandArgs(trailingOnly = TRUE)
 argLen <- length(arg)
-if (argLen == 3) {
+if (argLen == 6) {
     arg_b <- as.integer(arg[1]) # total b bits
     arg_n <- as.integer(arg[2]) # total n binary codes
     arg_t <- as.double(arg[3]) # number of bits for bit count
+    arg_s <- as.integer(arg[4]) # number of sampled dimensions
+    worst_weight <- as.double(arg[5])
+    best_weight <- as.double(arg[6])
 
 
-    first_ele = c(1:min(arg_b, ceiling(log2(arg_n)) + 1))
-    second_ele = c(min(arg_b, ceiling(log2(arg_n)) + 1):arg_b)
+    first_ele = c(1:min(arg_b, arg_s, ceiling(log2(arg_n)) + 1))
+    second_ele = c(min(arg_b, arg_s, ceiling(log2(arg_n)) + 1):(arg_b - arg_s))
 
     print(first_ele)
     print(second_ele)
 
-    worst_weight = 0.03
-    best_weight = 0.97
+    #worst_weight = 0.03
+    #best_weight = 0.97
 
-    first_sum = sum(2 ^ first_ele) * worst_weight + sum(2 * length(first_ele)) * best_weight
+    first_sum = sum(2 ^ first_ele) * worst_weight + (2 * length(first_ele)) * best_weight
 
-    second_sum = sum(rep(arg_n + 1, (arg_b - log2(arg_n)))) * worst_weight + sum(2 * length(second_ele)) * best_weight
+    second_sum = sum(rep(arg_n + 1, (arg_b - log2(arg_n)))) * worst_weight + (2 * length(second_ele)) * best_weight
+
+    sampled_sum = (2 * arg_s)
 
     print("result:")
-    compressed_bits = (first_sum + second_sum) * arg_t
+    compressed_bits = (first_sum + second_sum + sampled_sum) * arg_t 
     original_bits = as.double(arg_b) * arg_n
 
     print(paste("original: ", original_bits, sep = ''))
